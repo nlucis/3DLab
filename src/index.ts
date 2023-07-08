31b17f60870e3f3398a606aa3ba4de0e7afe9c7a
zon-creativeworks <arjay.jones@nlucis.dev>
2023-07-08 11:33:26 -0700

    set the iris and arms to always be facing the camera

--- src/index.ts
+++ src/index.ts
@@ -116,8 +116,6 @@ window.addEventListener('DOMContentLoaded', () => {
     thickness: 20,
   });
   const IRIS = new THREE.Mesh(gIRIS, mIRIS);
-  onAnimate.push(() => IRIS.lookAt(Camera.position));
-  Scene.add(IRIS);
 
   // Alpha & Omega arms
   const gArms = new THREE.CapsuleGeometry(0.3, 16);
@@ -131,23 +129,12 @@ window.addEventListener('DOMContentLoaded', () => {
   AlphaArm.position.set(0, +16 + 6, 0);
   OmegaArm.position.set(0, -16 - 6, 0);
 
-  onAnimate.push(() => {
-    // AlphaArm.lookAt(IRIS.position);
-    AlphaArm.rotation.set(0, 0, 0);
-    AlphaArm.position.set(0, +16+6 - Camera.rotation.y, 0);
-    // OmegaArm.lookAt(IRIS.position);
-  });
+  const statics = new THREE.Group();
+  statics.add(IRIS, AlphaArm, OmegaArm);
+  onAnimate.push(() => statics.lookAt(Camera.position));
+  Scene.add(statics);
 
-  console.debug(
-    '\nAlpha Arm - Position:',AlphaArm.position,
-    '\nCamera - Position',Camera.position,
-    '\nAlpha Arm - Rotation',AlphaArm.rotation,
-    '\nCamera - Rotation',Camera.rotation
-  );
-  Scene.add(
-    AlphaArm,
-    OmegaArm
-  );
+
 
   // Point Sphere
   const gPoints = new THREE.IcosahedronGeometry(9, 12);
