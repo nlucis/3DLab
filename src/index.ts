73c509b97e43e569ccaf3c99fb85662fc2468687
zon-creativeworks <arjay.jones@nlucis.dev>
2023-07-07 19:54:52 -0700

    added arms

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { TAARenderPass } from 'three/examples/jsm/postprocessing/TAARenderPass';
 import {UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPass';
 import {AfterimagePass } from 'three/examples/jsm/postprocessing/AfterimagePass';
-import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass';
+import { 
+  HalftonePass, 
+  HalftonePassParameters 
+} from 'three/examples/jsm/postprocessing/HalftonePass';
 import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass';
 import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass';
 
@@ -34,17 +37,38 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
   // camera...
-  Camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1e-3, 1e+3);
+  Camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1e-1, 1e+3);
   Camera.position.set(0, 0, -100);
 
   // Scene...
-  Scene.background = new THREE.Color(0.003, 0.006, 0.009);
-  Scene.fog = new THREE.FogExp2( 0x000104, 0.0000675 );
 
-  const portalGeom = new THREE.TorusGeometry(12);
-  const portalMats = new THREE.MeshBasicMaterial();
-  const PORTAL = new THREE.Mesh(portalGeom, portalMats);
-  // Scene.add(PORTAL);
+  // Tints entire scene color to red for nighttime
+  let redShiftMode = false;
+  if (redShiftMode) {
+    Scene.fog = new THREE.FogExp2( 0xFF0000, 0.012 );
+    Scene.background = new THREE.Color(0, 0, 0);
+  } else {
+    Scene.background = new THREE.Color(0.024, 0.012, 0.024);
+  }
+  
+  // Post-Processing
+  const composer = new EffectComposer(Renderer);
+  composer.addPass(new TAARenderPass(Scene, Camera, 0xFFFFFF, 1));
+
+  const bloom = new UnrealBloomPass(
+    new THREE.Vector2(), 
+    /* Strength */0.369, 
+    /* Radius   */0.222, 
+    /* Threshold*/0.001
+  );
+  const afterImage = new AfterimagePass(0.6);
+  const smaa = new SMAAPass(window.innerWidth, window.innerHeight);
+  const crt = new FilmPass(0.6, 0.3, (window.innerHeight / 2) * 3, 0);
+  
+  composer.addPass(bloom);
+  composer.addPass(smaa);
+  composer.addPass(afterImage);
+  composer.addPass(crt);
   
   // Load the prefab design
   const loader = new GLTFLoader();
@@ -58,79 +82,76 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   // Attempt at sphere with subsurface scattering
-  // const gSphere = new THREE.SphereGeometry(12, 36, 36);
-  // const mSphere = new THREE.MeshPhysicalMaterial({
-  //   name: 'sphere',
-  //   opacity: 0.72,
-  //   color: 0x1A1A27,
-  //   transparent: true,
+  const gSphere = new THREE.SphereGeometry(9, 36, 36);
+  const mSphere = new THREE.MeshPhysicalMaterial({
+    name: 'sphere',
+    opacity: 0.36,
+    color: 0x2A1CAF,
+    transparent: true,
 
-  //   emissive: 0xFFFFFF,
-  //   emissiveIntensity: 0.24,
+    emissive: 0xFFFFFF,
+    emissiveIntensity: 0.24,
     
-  // // a slight clearcoat creates a softened edge that works well with bloom
-  //   clearcoat: 1,
-  //   clearcoatRoughness: 0.12,
-  //   transmission: 0.6,
-  // });
+  // a slight clearcoat creates a softened edge that works well with bloom
+    clearcoat: 1,
+    clearcoatRoughness: 0.12,
+    transmission: 0.6,
+    side: THREE.BackSide
+  });
+  const Sphere = new THREE.Mesh(gSphere, mSphere);
 
   // Lights and FX
-  const lAmbient = new THREE.AmbientLight(0xFFFFFF, 36);
-  lAmbient.position.set(0, 0, 10);
+  const lAmbient = new THREE.AmbientLight(0xAC9CEF, 36);
   Scene.add(
+    Sphere,
     lAmbient, 
   );
-
-  // Post-Processing
-  const composer = new EffectComposer(Renderer);
-  composer.addPass(new TAARenderPass(Scene, Camera, 0xFFFFFF, 1));
-
-  const bloom = new UnrealBloomPass(
-    new THREE.Vector2(), 
-    /* Strength */0.369, 
-    /* Radius   */0.222, 
-    /* Threshold*/0.001
-  );
-  composer.addPass(bloom);
-  
-  const afterImage = new AfterimagePass(0.6);
-  composer.addPass(afterImage);
-
-  const smaa = new SMAAPass(window.innerWidth, window.innerHeight);
-  composer.addPass(smaa);
-
-  const crt = new FilmPass(0.6, 0.3, (window.innerHeight / 2) * 3, 0);
-  composer.addPass(crt);
-
-  const dotScreen = new DotScreenPass(new THREE.Vector2(-3, +3), THREE.MathUtils.degToRad(45), 1.3);
-  composer.addPass(dotScreen);
 
   // IRIS Ring
   const gIRIS = new THREE.TorusGeometry(12, 1, 64, 64);
   const mIRIS = new THREE.MeshPhysicalMaterial({
     name: 'contactRing',
     opacity: 0.72,
-    color: 0x000000,
+    color: 0xFFECEE,
     transparent: true,
 
-    emissive: 0xFCE2BC,
-    emissiveIntensity: 0.6,
+    emissive: 0xFFFFFF,
+    emissiveIntensity: 0.24,
 
     clearcoat: 1,
-    clearcoatRoughness: 0.12,
-    transmission: 0.24
+    clearcoatRoughness: 0.72,
+    transmission: 0.36,
+    side: THREE.DoubleSide,
+    thickness: 20,
   });
   const IRIS = new THREE.Mesh(gIRIS, mIRIS);
   Scene.add(IRIS);
 
-  // Points and Particles
+  // Alpha & Omega arms
+  const gArms = new THREE.CapsuleGeometry(0.3, 16);
+  const mArms = new THREE.MeshPhysicalMaterial({
+    color: 0xFF6C11,
+    emissive: 0xFFAC00,
+    emissiveIntensity: 0.36,
+  });
+  const AlphaArm = new THREE.Mesh(gArms, mArms);
+  const OmegaArm = new THREE.Mesh(gArms, mArms);
+
+  AlphaArm.position.set(0, +16 + 6, 0);
+  OmegaArm.position.set(0, -16 - 6, 0);
+
+  Scene.add(
+    AlphaArm,
+    OmegaArm
+  );
+
+  // Point Sphere
   const gPoints = new THREE.IcosahedronGeometry(9, 12);
   const mPoints = new THREE.PointsMaterial({
     name: 'point-sphere',
-    color: 0x72BCFF,
-    size: 0.3,
-    opacity: 0.3,
-    side: THREE.DoubleSide,
+    color: redShiftMode ? 0xF27F4C : 0x7C7C7C,
+    size: 0.1,
+    opacity: 0.6,
     blending: THREE.AdditiveBlending,
   });
 
@@ -139,14 +160,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Simulate vox flashing
   setInterval(() => {
-    mPoints.opacity = THREE.MathUtils.randInt(opacityMin, opacityMax);
+    mPoints.opacity = THREE.MathUtils.randFloat(opacityMin, opacityMax);
+    mPoints.size = THREE.MathUtils.randFloat(0.3, 0.6);
   }, 64);
 
   const GridPoints = new THREE.Points(gPoints, mPoints);
   onAnimate.push(() => {
-    GridPoints.rotation.x += 0.003;
+    GridPoints.rotation.x += 0.006;
     GridPoints.rotation.y -= 0.003;
-    GridPoints.rotation.z += 0.003;
+    GridPoints.rotation.z += 0.009;
   });
   Scene.add(GridPoints);
   console.debug(GridPoints);
