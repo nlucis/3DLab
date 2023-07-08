5e91281c3a6c25c585fd862780ae1cb9c6618912
zon-creativeworks <arjay.jones@nlucis.dev>
2023-07-07 18:13:11 -0700

    pretty neat looking cyber brain

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { TAARenderPass } from 'three/examples/jsm/postprocessing/TAARenderPass';
 import {UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPass';
 import {AfterimagePass } from 'three/examples/jsm/postprocessing/AfterimagePass';
+import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass';
+import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass';
 import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass';
 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
@@ -56,46 +58,39 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   // Attempt at sphere with subsurface scattering
-  const gSphere = new THREE.SphereGeometry(12, 36, 36);
-  const mSphere = new THREE.MeshPhysicalMaterial({
-    name: 'sphere',
-    opacity: 1,
-    color: 0x1A1A27,
-    transparent: true,
+  // const gSphere = new THREE.SphereGeometry(12, 36, 36);
+  // const mSphere = new THREE.MeshPhysicalMaterial({
+  //   name: 'sphere',
+  //   opacity: 0.72,
+  //   color: 0x1A1A27,
+  //   transparent: true,
 
-    emissive: 0xFFFFFF,
-    emissiveIntensity: 0.1,
+  //   emissive: 0xFFFFFF,
+  //   emissiveIntensity: 0.24,
     
-    clearcoat: 1,
-    clearcoatRoughness: 0.12,
-    side: THREE.FrontSide,
-    transmission: 0.6,
-  });
-  const Sphere = new THREE.Mesh(gSphere, mSphere);
-  const lAmbient = new THREE.AmbientLight(0xFFFFFF, 20);
-  const lPoint = new THREE.PointLight(0xFF00AC, 3, 0, 1);
-  Camera.add(lPoint);
+  // // a slight clearcoat creates a softened edge that works well with bloom
+  //   clearcoat: 1,
+  //   clearcoatRoughness: 0.12,
+  //   transmission: 0.6,
+  // });
+
+  // Lights and FX
+  const lAmbient = new THREE.AmbientLight(0xFFFFFF, 36);
   lAmbient.position.set(0, 0, 10);
-  lPoint.position.set(16, 0, 0);
   Scene.add(
-    Sphere, 
     lAmbient, 
-    lPoint
   );
 
   // Post-Processing
   const composer = new EffectComposer(Renderer);
   composer.addPass(new TAARenderPass(Scene, Camera, 0xFFFFFF, 1));
 
-
-
   const bloom = new UnrealBloomPass(
-    new THREE.Vector2(window.innerWidth, window.innerHeight), 
-    /* Strength */0.618, 
-    /* Radius   */0.369, 
+    new THREE.Vector2(), 
+    /* Strength */0.369, 
+    /* Radius   */0.222, 
     /* Threshold*/0.001
   );
-  console.debug(bloom.bloomTintColors);
   composer.addPass(bloom);
   
   const afterImage = new AfterimagePass(0.6);
@@ -103,6 +98,58 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const smaa = new SMAAPass(window.innerWidth, window.innerHeight);
   composer.addPass(smaa);
+
+  const crt = new FilmPass(0.6, 0.3, (window.innerHeight / 2) * 3, 0);
+  composer.addPass(crt);
+
+  const dotScreen = new DotScreenPass(new THREE.Vector2(-3, +3), THREE.MathUtils.degToRad(45), 1.3);
+  composer.addPass(dotScreen);
+
+  // IRIS Ring
+  const gIRIS = new THREE.TorusGeometry(12, 1, 64, 64);
+  const mIRIS = new THREE.MeshPhysicalMaterial({
+    name: 'contactRing',
+    opacity: 0.72,
+    color: 0x000000,
+    transparent: true,
+
+    emissive: 0xFCE2BC,
+    emissiveIntensity: 0.6,
+
+    clearcoat: 1,
+    clearcoatRoughness: 0.12,
+    transmission: 0.24
+  });
+  const IRIS = new THREE.Mesh(gIRIS, mIRIS);
+  Scene.add(IRIS);
+
+  // Points and Particles
+  const gPoints = new THREE.IcosahedronGeometry(9, 12);
+  const mPoints = new THREE.PointsMaterial({
+    name: 'point-sphere',
+    color: 0x72BCFF,
+    size: 0.3,
+    opacity: 0.3,
+    side: THREE.DoubleSide,
+    blending: THREE.AdditiveBlending,
+  });
+
+  let opacityMax = 2.2;
+  let opacityMin = 0.6;
+
+  // Simulate vox flashing
+  setInterval(() => {
+    mPoints.opacity = THREE.MathUtils.randInt(opacityMin, opacityMax);
+  }, 64);
+
+  const GridPoints = new THREE.Points(gPoints, mPoints);
+  onAnimate.push(() => {
+    GridPoints.rotation.x += 0.003;
+    GridPoints.rotation.y -= 0.003;
+    GridPoints.rotation.z += 0.003;
+  });
+  Scene.add(GridPoints);
+  console.debug(GridPoints);
 
 
   // Action!
